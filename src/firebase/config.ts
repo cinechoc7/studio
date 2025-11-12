2fda6f72d33876306049cd1a5e2df9183210e059
ciné choc <cinechoc7@gmail.com>
2025-11-12 23:37:18 +0000

    Nom du projet Firebase app ID du projet studio-4685666846-d14d6 Numéro d

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyAQmZi1OwbxQjQginHANkMDC7L4bYIUP7c",
   "authDomain": "studio-4685666846-d14d6.firebaseapp.com",
   "projectId": "studio-4685666846-d14d6",
-  "storageBucket": "studio-4685666846-d14d6.appspot.com",
+  "storageBucket": "studio-4685666846-d14d6.firebasestorage.app",
   "messagingSenderId": "466949142415",
   "appId": "1:466949142415:web:b16806a61ce01192243dfa"
 };
